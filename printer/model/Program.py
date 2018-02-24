a3e0fb1f6134a4ba4d9907096ca680d75c943bd3
JewelYueng <414608192@qq.com>
2018-02-24 12:31:20 +0800

    fix(program): 更改program的数据字段

--- printer/model/Program.py
+++ printer/model/Program.py
@@ -12,13 +12,17 @@ class Program(DB.Model):
     __tablename__ = 'program'
     customer_id = DB.Column(DB.Integer, DB.ForeignKey('company.id'))
     program_id = DB.Column(DB.Integer, primary_key=True, autoincrement=True, nullable=False)
+    painting_id = DB.Column(DB.Integer, DB.ForeignKey('painting.id'))
+    product_id = DB.Column(DB.Integer, DB.ForeignKey('product.id'))
     painting_program = DB.Column(DB.Integer)
     device_program = DB.Column(DB.Integer)
     dry_program = DB.Column(DB.Integer)
 
 
-    def __init__(self, customer_id, painting_program, device_program, dry_program):
-        self.customer_id =customer_id
+    def __init__(self, customer_id, product_id, painting_id, painting_program, device_program, dry_program):
+        self.customer_id = customer_id
+        self.product_id = product_id
+        self.painting_id = painting_id
         self.painting_program = painting_program
         self.device_program = device_program
         self.dry_program = dry_program
